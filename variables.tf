be50d495ab76d57b9f3f4298371ec2c35da6ff3f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:37:54 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "lb_backend_address_pools" {
+  description = <<EOT
+Map of lb_backend_address_pools, attributes below
+Required:
+    - loadbalancer_id
+    - name
+Optional:
+    - synchronous_mode
+    - virtual_network_id
+    - tunnel_interface (block):
+        - identifier (required)
+        - port (required)
+        - protocol (required)
+        - type (required)
+EOT
+
+  type = map(object({
+    loadbalancer_id    = string
+    name               = string
+    synchronous_mode   = optional(string)
+    virtual_network_id = optional(string)
+    tunnel_interface = optional(object({
+      identifier = number
+      port       = number
+      protocol   = string
+      type       = string
+    }))
+  }))
+}
+
